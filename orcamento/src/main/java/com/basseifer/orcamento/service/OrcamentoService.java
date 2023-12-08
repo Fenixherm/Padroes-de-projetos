ac577e238b70ebf3797a5b8963ea4304bc493a9a
Guilherme <guibah78@gmail.com>
2023-12-08 16:47:50 -0300

    Alteração em alguns métodos

--- orcamento/src/main/java/com/basseifer/orcamento/service/OrcamentoService.java
+++ orcamento/src/main/java/com/basseifer/orcamento/service/OrcamentoService.java
@@ -1,43 +1,29 @@
 package com.basseifer.orcamento.service;
 
 import com.basseifer.orcamento.model.Orcamento;
+import com.basseifer.orcamento.model.OrcamentoRepository;
+import com.basseifer.orcamento.model.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
+import java.util.Optional;
 
 public class OrcamentoService implements IOrcamentoService{
+    @Autowired
+    private OrcamentoRepository orcamentoRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     public Iterable<Orcamento> buscarTodos() {
-        return null;
+        return orcamentoRepository.findAll();
     }
 
     @Override
-    public Iterable<Orcamento> buscarPorRomaneio(Integer romaneio) {
-        return null;
-    }
+    public Orcamento buscarPorId(Long id) {
+        Optional<Orcamento> orcamento = orcamentoRepository.findById(id);
 
-    @Override
-    public Iterable<Orcamento> buscarPorObra(String obra) {
-        return null;
-    }
-
-    @Override
-    public Iterable<Orcamento> buscarPorCliente(String cliente) {
-        return null;
-    }
-
-    @Override
-    public Iterable<Orcamento> buscaPorSituacao(String situacao) {
-        return null;
-    }
-
-    @Override
-    public Iterable<Orcamento> buscaPorDataChegada(Date dataChegada) {
-        return null;
-    }
-
-    @Override
-    public Iterable<Orcamento> buscaPorDataSaida(Date dataSaida) {
-        return null;
+        return orcamento.get();
     }
 
     @Override
